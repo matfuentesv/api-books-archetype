37b01c96ef26d212f3b436ed3fceea29b0e0dd1c
O009920 <mfuentes@forum.cl>
2025-01-10 00:36:09 -0300

    avance servicio

---
+++ src/main/java/cl/company/controller/DefaultController.java
@@ -0,0 +1,17 @@
+package cl.company.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class DefaultController {
+
+    @GetMapping("/mensaje")
+    public String mensaje() {
+
+        System.out.println("Backend llamado");
+        return "{\"mensaje\": \"Integración OK al backend\"}";
+    }
+}
